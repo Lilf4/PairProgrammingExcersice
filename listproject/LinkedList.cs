3f207350c4e2d82f759a3082b225f7db46d7a951
lilf4 <danielfandersen@hotmail.com>
2024-11-06 10:17:02 +0100

    Initial commit

---
+++ listproject/LinkedList.cs
@@ -0,0 +1,34 @@
+using System.Linq.Expressions;
+
+namespace LinkedListProject{
+	public class LinkedList{
+		public Element? first;
+		public void addFirst(int num){
+			var new_element = new Element(num);
+
+			if(first != null){
+				new_element.next = new Element(first);
+			}
+
+			first = new_element;
+		}
+
+		public void removeFirst(){
+			first = null;
+		}
+	}
+
+	public class Element{
+		public int data;
+		public Element? next;
+		public Element(int new_data){
+			data = new_data;
+		}
+		public Element(Element to_copy = null){
+			if (to_copy != null){
+				data = to_copy.data;
+				next = to_copy.next;
+			}
+		}
+	}
+}
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,10 +1,50 @@
 namespace test;
+using LinkedListProject;
 
 public class UnitTest1
 {
     [Fact]
     public void LinkedListConstructorTest()
     {
-
+		var list = new LinkedList();
+		Assert.NotNull(list);
     }
+
+	[Fact]
+    public void LinkedListNewFirstElementStructureTest()
+    {
+		var list = new LinkedList();
+		list.addFirst(2);
+		Assert.Equal(2, list.first.data);
+		Assert.Null(list.first.next);
+    }
+
+	[Fact]
+	public void LinkedListAddOneElement(){
+		var list = new LinkedList();
+		list.addFirst(1);
+		Assert.Equal(1, list.first.data);
+	}
+
+	[Fact]
+	public void LinkedListAddMultipleElements(){
+		var list = new LinkedList();
+		list.addFirst(3);
+		list.addFirst(5);
+		list.addFirst(1);
+
+		Assert.Equal(1, list.first.data);
+		Assert.Equal(5, list.first.next.data);
+		Assert.Equal(3, list.first.next.next.data);
+	}
+
+	[Fact]
+	public void LinkedListRemoveOneElement(){
+		var list = new LinkedList();
+		list.addFirst(5);
+		Assert.Equal(5, list.first.data);
+		
+		list.removeFirst();
+		Assert.Null(list.first);
+	}
 }
