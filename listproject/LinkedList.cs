0b21bc62474bf9604ea9790c6dbfc4014d24a435
lilf4 <danielfandersen@hotmail.com>
2024-11-06 12:09:17 +0100

    Added IsSorted function

--- listproject/LinkedList.cs
+++ listproject/LinkedList.cs
@@ -1,15 +1,19 @@
 using System.Linq.Expressions;
+using System.Text;
 
-namespace LinkedListProject{
-	public class LinkedList{
+namespace LinkedListProject
+{
+	public class LinkedList
+	{
 		public Element? first;
 
-		public int Count {get{return count;}}
+		public int Count { get { return count; } }
 		private int count = 0;
-		public void addFirst(int num){
+		public void addFirst(int num)
+		{
 			var new_element = new Element(num);
-
-			if(first != null){
+			if (first != null)
+			{
 				new_element.next = new Element(first);
 			}
 
@@ -17,22 +21,66 @@ namespace LinkedListProject{
 			count++;
 		}
 
-		public void removeFirst(){
+		public void removeFirst()
+		{
 			first = first.next;
 			count--;
 		}
-	}
 
-	public class Element{
-		public int data;
-		public Element? next;
-		public Element(int new_data){
-			data = new_data;
+		public override string ToString()
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.Append("LinkedList: ");
+			if(first != null){
+				var currElement = first;
+				for(int i = 0; i < Count; i++){
+					stringBuilder.Append(currElement.data.ToString());
+					if (currElement.next != null){
+						stringBuilder.Append(", ");
+						currElement = currElement.next;
+					}
+				}
+			}
+			else{
+				stringBuilder.Append("Empty");
+			}
+			return stringBuilder.ToString();
 		}
-		public Element(Element to_copy = null){
-			if (to_copy != null){
-				data = to_copy.data;
-				next = to_copy.next;
+
+		public void Sort(){
+			if(IsSorted()) return;
+
+			
+		}
+
+		public bool IsSorted(){
+			if(first == null) return true;
+			var currElement = first;
+			for(int i = 0; i < Count; i++){
+				if(currElement.next == null) return true;
+				if(currElement.data > currElement.next.data){
+					return false;
+				}
+				currElement = currElement.next;
+			}
+			return true;
+		}
+
+		public class Element
+		{
+			public int data;
+			public Element? next;
+			public Element(int new_data)
+			{
+				data = new_data;
+			}
+			public Element(Element to_copy = null)
+			{
+				if (to_copy != null)
+				{
+					data = to_copy.data;
+					next = to_copy.next;
+				}
 			}
 		}
 	}
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -7,14 +7,14 @@ using LinkedListProject;
 public class UnitTest1
 {
     [Fact]
-    public void LinkedListConstructorTest()
+    public void ConstructorTest()
     {
 		var list = new LinkedList();
 		Assert.NotNull(list);
     }
 
 	[Fact]
-    public void LinkedListNewFirstElementStructureTest()
+    public void NewFirstElementStructureTest()
     {
 		var list = new LinkedList();
 		list.addFirst(2);
@@ -23,14 +23,14 @@ public class UnitTest1
     }
 
 	[Fact]
-	public void LinkedListAddOneElement(){
+	public void AddOneElement(){
 		var list = new LinkedList();
 		list.addFirst(1);
 		Assert.Equal(1, list.first.data);
 	}
 
 	[Fact]
-	public void LinkedListAddMultipleElements(){
+	public void AddMultipleElements(){
 		var list = new LinkedList();
 		list.addFirst(3);
 		list.addFirst(5);
@@ -42,7 +42,7 @@ public class UnitTest1
 	}
 
 	[Fact]
-	public void LinkedListRemoveOneElement(){
+	public void RemoveOneElement(){
 		var list = new LinkedList();
 		list.addFirst(5);
 		Assert.Equal(5, list.first.data);
@@ -81,5 +81,55 @@ public class UnitTest1
 		list.removeFirst();
 
 		Assert.Equal(1,list.Count);
+	}
+
+	[Fact]
+	public void ToStringTest()
+	{
+		var list = new LinkedList();
+		Assert.Equal("LinkedList: Empty", list.ToString());
+
+		list.addFirst(3);
+		list.addFirst(8);
+		list.addFirst(1);
+
+		Assert.Equal("LinkedList: 1, 8, 3", list.ToString());
+	}
+
+	[Theory]
+	[InlineData (1, 2, 3, true)]
+	[InlineData (4, 3, 2, false)]
+	public void IsSorted(int a, int b, int c, bool result){
+		var list = new LinkedList();
+
+		list.addFirst(c);
+		list.addFirst(b);
+		list.addFirst(a);
+
+		Assert.Equal(result, list.IsSorted());
+	}
+	[Fact] 
+	public void IsEmptySorted(){
+		var list = new LinkedList();
+		Assert.True(list.IsSorted());
+	}
+
+	[Fact]
+	public void LLSortEmpty(){
+		var list = new LinkedList();
+		list.Sort();
+		Assert.True(list.IsSorted());
+	}
+
+	[Fact]
+	public void LLSort(){
+		var list = new LinkedList();
+		list.addFirst(5);
+		list.addFirst(2);
+		list.addFirst(9);
+		list.addFirst(1);
+		list.addFirst(4);
+		list.Sort();
+		Assert.True(list.IsSorted());
 	}
 }
